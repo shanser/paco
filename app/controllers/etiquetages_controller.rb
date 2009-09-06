97062b0c28dc84c36589c15f594cda811f95ae51
Stéphane Hanser <shanser@captive-studio.com>
2009-09-06 12:25:58 +0200

    refactoring de etiquetagescontroller : on ne passe plus une liste d'étiquetages mais un hash qui groupe les étiquetages par tâche

--- app/controllers/etiquetages_controller.rb
+++ app/controllers/etiquetages_controller.rb
@@ -3,8 +3,11 @@ class EtiquetagesController < ApplicationController
   # GET /etiquetages
   # GET /etiquetages.xml
   def index
-    @etiquetages = Etiquetage.all(:include => :tag, :order => 'tags.description')
-
+    etiquetages = Etiquetage.all(:include => :tag, :order => 'tags.description')
+    tags = etiquetages.collect(&:tag).uniq
+    @etiquetages = {}
+    tags.each {|tag| @etiquetages[tag] = etiquetages.select{|e| e.tag == tag}}
+    
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @etiquetages }
--- test/functional/etiquetages_controller_test.rb
+++ test/functional/etiquetages_controller_test.rb
@@ -2,9 +2,11 @@ require 'test_helper'
 
 class EtiquetagesControllerTest < ActionController::TestCase
   test "should get index" do
-    Factory(:etiquetage)
+    tags = [1, 2].map{Factory :tag}
+    e1, e2, e3 = [0, 1, 0].map{|n| Factory :etiquetage, :tag => tags[n]}
+    t1, t2 = tags
     get :index
     assert_response :success
-    assert_not_nil assigns(:etiquetages)
+    assert_equal({t1 => [e1, e3], t2 => [e2]}, assigns(:etiquetages))
   end
 end
