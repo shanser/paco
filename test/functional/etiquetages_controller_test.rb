5b4bdda54344e821c738a4d1210dbc99f8efbb7a
Stéphane Hanser <shanser@captive-studio.com>
2009-09-15 18:00:31 +0200

    refactorigne du setup de l'index pour qu'il soit plus compréhensible

--- test/functional/etiquetages_controller_test.rb
+++ test/functional/etiquetages_controller_test.rb
@@ -2,9 +2,8 @@ require 'test_helper'
 
 class EtiquetagesControllerTest < ActionController::TestCase
   test "should get index" do
-    tags = [1, 2].map{Factory :tag}
-    e1, e2, e3 = [0, 1, 0].map{|n| Factory :etiquetage, :tag => tags[n]}
-    t1, t2 = tags
+    t1, t2 = Factory(:tag), Factory(:tag)
+    e1, e2, e3 = [t1, t2, t1].map{|tag| Factory :etiquetage, :tag => tag}
     get :index
     assert_response :success
     assert_equal({t1 => [e1, e3], t2 => [e2]}, assigns(:etiquetages))
